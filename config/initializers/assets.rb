fd8674db69e7dcf5ff4385629a778b2cb8b72521
hiroaki_seto <hiroaki5141616@me.com>
2024-01-18 22:53:25 +0900

    [fix]Rubocop-Aの実行

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,3 @@
-Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
+# frozen_string_literal: true
+
+Rails.application.config.assets.precompile += %w[bootstrap.min.js popper.js]
