a1d60d6ab821607e8024e7e0126dff09c0668bae
hiroaki_seto <hiroaki5141616@me.com>
2024-01-18 23:33:59 +0900

    [fix]routes.rbにdeviseの読み込み記述を追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   # Add Devise routes for user authentication
-  # devise_for :users
+  devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
