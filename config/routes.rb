74badbf25959681b4834400ba00761e84ebce1a9
hiroaki_seto <hiroaki5141616@me.com>
2024-01-18 23:31:31 +0900

    [add]devise関連のファイルを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
+
+  # Add Devise routes for user authentication
+  # devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
