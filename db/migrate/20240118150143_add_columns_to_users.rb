c00136b3a49cefc7a621accd0635f24b0a40a750
hiroaki_seto <hiroaki5141616@me.com>
2024-01-19 13:27:21 +0900

    [fix]deviseと重複するカラム追加項目をマイグレーションから削除

--- db/migrate/20240118150143_add_columns_to_users.rb
+++ db/migrate/20240118150143_add_columns_to_users.rb
@@ -3,7 +3,6 @@ class AddColumnsToUsers < ActiveRecord::Migration[7.1]
     add_column :users, :name, :string
     add_column :users, :phone_number, :string
     add_column :users, :birthdate, :date
-    add_column :users, :password, :string
     add_column :users, :BeeBits_id, :string
   end
 end
