897fededd63d97a3af39ab7612a3fb3b2f5175b9
hiroaki_seto <hiroaki5141616@me.com>
2024-01-22 21:23:52 +0900

    アカウント作成からログアウトまでの流れを作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   # Add Devise routes for user authentication
   devise_for :users
 
+  # Redirect to login page after sign up
+  devise_scope :user do
+    post 'users/sign_up', to: 'devise/sessions#new'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
