76d9b4b8301bd7ade15abf13db5fb5e6c9f0f0f4
hiroaki_seto <hiroaki5141616@me.com>
2024-01-26 23:46:14 +0900

    Homeコントローラーと関連ファイルを削除

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class HomeController < ApplicationController
-  def index
-    @users = User.all
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
-  #テスト用
-  get 'home/index', to: 'home#index', as: 'home_index'
-
   root 'timelines#index'
 end
