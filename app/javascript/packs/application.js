3698903246256b291a5e2788964d0496177f0404
hiroaki_seto <hiroaki5141616@me.com>
2024-01-27 12:05:23 +0900

    TestComponent.jsxファイルの追加/application.jsの追加

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyComponent from '../components/MyComponent';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<MyComponent />, document.getElementById('react-root'));
+});
