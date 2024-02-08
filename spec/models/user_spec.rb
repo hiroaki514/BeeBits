e0c184e9687270121d06337d8c24ab146bd875fa
hiroaki_seto <hiroaki5141616@me.com>
2024-02-08 12:41:11 +0900

    ユーザー登録のテスト項目を記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,99 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe '#validation' do
-    context '入力項目のバリデーション' do
-      it '名前の入力が必須であること' do
-        user = build(:user, name: '')
-        user.valid?
-        expect(user.errors.full_messages).to include('名前 を入力してください')
+    context '名前' do
+      it '入力が必須であること' do
+
+      end
+
+      it '2文字以上の入力であること' do
+
+      end
+
+      it '入力が50文字以内であること' do
+        
+      end
+    end
+
+    context 'メールアドレス' do
+      it '入力が必須であること' do
+        
+      end
+
+      it '有効な形式であること' do
+        
+      end
+
+      it '入力が254文字以内であること' do
+        
+      end
+    end
+
+    context '電話番号' do
+      it '入力が必須であること' do
+        pending
+      end
+
+      it '有効な形式であること' do
+        pending
+      end
+
+      it '日本国内の形式であること' do
+        pending
+      end
+    end
+
+    context '生年月日' do
+      it '入力が必須であること' do
+
+      end
+
+      it '有効な形式であること' do
+        
+      end
+
+      it '入力が15歳以上であること' do
+        
+      end
+    end
+
+    context 'BeeBitsユーザー名' do
+      it '入力が必須であること' do
+        
+      end
+
+      it '大文字小文字を区別せずに、同じBeeBitsユーザー名が存在しないこと' do
+        
+      end
+
+      it '入力が@を含めて15文字以内であること' do
+        
+      end
+
+      it '@(アットマーク)と_(アンダーバー)意外の記号が含まれないこと' do
+        
+      end
+
+      it '@(アットマーク)が2つ以上含まれないこと' do
+        
+      end
+    end
+
+    context 'パスワード' do
+      it '入力が必須であること' do
+        
+      end
+
+      it '入力が8文字以上であること' do
+        
+      end
+
+      it '入力が8文字以上かつ英数字が含まれること' do
+        
+      end
+
+      it '入力が128文字以内であること' do
+        
       end
     end
   end
