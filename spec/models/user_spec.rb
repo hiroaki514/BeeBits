62cc363b137b300bd7141b8ed8159e6d10f5c424
hiroaki_seto <hiroaki5141616@me.com>
2024-02-09 02:41:30 +0900

    user_spec.rbテスト記述途中

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,71 +6,91 @@ RSpec.describe User, type: :model do
   describe '#validation' do
     context '名前' do
       it '入力が必須であること' do
-
+        user = build(:user, name: '')
+        user.valid?
+        expect(user.errors.full_messages).to include('名前 を入力してください')
       end
 
       it '2文字以上の入力であること' do
-
+        user = build(:user, name: '蜂')
+        user.valid?
+        expect(user.errors.full_messages).to include('名前 は2文字以上で入力してください')
       end
 
       it '入力が50文字以内であること' do
-        
+        user = build(:user, name: 'a' * 51)
+        user.valid?
+        expect(user.errors.full_messages).to include('名前 は50文字以内で入力してください')
       end
     end
 
     context 'メールアドレス' do
       it '入力が必須であること' do
-        
+        user = build(:user, email: '')
+        user.valid?
+        expect(user.errors.full_messages).to include('メールアドレス を入力してください')
       end
 
       it '有効な形式であること' do
-        
+        user = build(:user, email: 'beebits@com')
+        user.valid?
+        expect(user.errors.full_messages).to include('メールアドレス は有効な形式で入力してください')
       end
 
       it '入力が254文字以内であること' do
-        
+        user = build(:user, email: 'b' * 241 + '@example.com')
+        user.valid?
+        expect(user.errors.full_messages).to include('メールアドレス は254文字以内で入力してください')
       end
     end
 
     context '電話番号' do
       it '入力が必須であること' do
-        pending
-      end
 
-      it '有効な形式であること' do
-        pending
       end
 
       it '日本国内の形式であること' do
-        pending
+        user = build(:user, phone_number: '01012345678')
+        user.valid?
+        expect(user.errors.full_messages).to include('電話番号 は日本の携帯電話番号の形式で入力してください')
       end
     end
 
     context '生年月日' do
       it '入力が必須であること' do
-
+        user = build(:user, birthdate: '')
+        user.valid?
+        expect(user.errors.full_messages).to include('生年月日 を入力してください')
       end
 
       it '有効な形式であること' do
-        
+        user = build(:user, birthdate: '1800-01-01')
+        user.valid?
+        expect(user.errors.full_messages).to include('生年月日 の入力が正しくありません')
       end
 
       it '入力が15歳以上であること' do
-        
+        user = build(:user, birthdate: three_days_ago - 15.years - 1.day)
+        user.valid?
+        expect(user.errors.full_messages).to include('生年月日 が15歳未満の方はご利用いただけません')
       end
     end
 
     context 'BeeBitsユーザー名' do
       it '入力が必須であること' do
-        
+        user = build(:user, beebits_name: '')
+        user.valid?
+        expect(user.errors.full_messages).to include('BeeBitsユーザー名 を入力してください')
       end
 
       it '大文字小文字を区別せずに、同じBeeBitsユーザー名が存在しないこと' do
-        
+
       end
 
       it '入力が@を含めて15文字以内であること' do
-        
+        user = build(:user, beebits_name: '@beebits_test123')
+        user.valid?
+        expect(user.errors.full_messages).to include('BeeBitsユーザー名 は文字以内で入力してください')
       end
 
       it '@(アットマーク)と_(アンダーバー)意外の記号が含まれないこと' do
@@ -84,7 +104,9 @@ RSpec.describe User, type: :model do
 
     context 'パスワード' do
       it '入力が必須であること' do
-        
+        user = build(:user, password: '')
+        user.valid?
+        expect(user.errors.full_messages).to include('パスワード を入力してください')
       end
 
       it '入力が8文字以上であること' do
