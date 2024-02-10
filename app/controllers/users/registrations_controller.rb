931d47159cca0c61c632a74106f2290850fdc0fc
hiroaki_seto <hiroaki5141616@me.com>
2024-02-10 16:22:29 +0900

    @の自動挿入用記述を削除:後ほどReactでの実装予定のため

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,19 +2,12 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController
-    before_action :configure_sign_up_params, only: [:create]
 
     private
 
     # Strong Parametersの設定
     def sign_up_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :phone_number, :birthdate, :beebits_name)
-    end
-
-    # 新規登録時のStrong Parametersを設定
-    def configure_sign_up_params
-      # beebits_nameの冒頭に＠を自動で追加する処理
-      params[:user][:beebits_name] = "@#{params[:user][:beebits_name]}" unless params[:user][:beebits_name].blank?
     end
 
     def account_update_params
