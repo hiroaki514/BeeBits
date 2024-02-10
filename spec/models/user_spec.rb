f3dc195715ad2b60b9b37fcc050c4b796196d583
hiroaki_seto <hiroaki5141616@me.com>
2024-02-10 18:02:54 +0900

    テスト項目追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,10 +37,18 @@ RSpec.describe User, type: :model do
         expect(user.errors.full_messages).to include('メールアドレス は有効な形式で入力してください')
       end
 
+      it '大文字小文字を区別せずに、同じメールアドレスが存在しないこと' do
+        user1 = create(:user, email: 'BeeBits@example.com')
+
+        user2 = build(:user, email: 'beebits@example.com')
+        user2.valid?
+        expect(user2.errors.full_messages).to include('メールアドレス はすでに存在しています')
+      end
+
       it '入力が254文字以内であること' do
-        user = build(:user, email: '
-          beebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebits@example.com
-        ')
+        user = build(:user, email: 
+          'beebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebits@example.com'
+        )
         user.valid?
         expect(user.errors.full_messages).to include('メールアドレス は254文字以内で入力してください')
       end
@@ -51,8 +59,14 @@ RSpec.describe User, type: :model do
 
       end
 
-      it '日本国内の形式であること' do
+      it '日本国内の電話番号であること' do
         user = build(:user, phone_number: '01012345678')
+        user.valid?
+        expect(user.errors.full_messages).to include('電話番号 は日本の携帯電話番号の形式で入力してください')
+      end
+
+      it '国際電話番号は無効であること' do
+        user = build(:user, phone_number: '+1234567890')
         user.valid?
         expect(user.errors.full_messages).to include('電話番号 は日本の携帯電話番号の形式で入力してください')
       end
@@ -76,6 +90,12 @@ RSpec.describe User, type: :model do
         user.valid?
         expect(user.errors.full_messages).to include('生年月日 が15歳未満の方はご利用いただけません')
       end
+
+      it '未来の日付は無効であること' do
+        user = build(:user, birthdate: Date.today + 1.day)
+        user.valid?
+        expect(user.errors.full_messages).to include('生年月日 の入力が正しくありません')
+      end
     end
 
     context 'BeeBitsユーザー名' do
@@ -86,21 +106,31 @@ RSpec.describe User, type: :model do
       end
 
       it '大文字小文字を区別せずに、同じBeeBitsユーザー名が存在しないこと' do
+        user1 = create(:user, beebits_name: '@Bee_Bits')
 
+        user2 = build(:user, beebits_name: '@bee_bits')
+        user2.valid?
+        expect(user2.errors.full_messages).to include('BeeBitsユーザー名 はすでに存在しています')
       end
 
       it '入力が@を含めて15文字以内であること' do
-        user = build(:user, beebits_name: 'beebits_test123')
+        user = build(:user, beebits_name: '@beebits_test123')
         user.valid?
-        expect(user.errors.full_messages).to include('BeeBitsユーザー名 は文字以内で入力してください')
+        expect(user.errors.full_messages).to include('BeeBitsユーザー名 は@を含め15文字以内で入力してください')
       end
 
       it '@(アットマーク)と_(アンダーバー)意外の記号が含まれないこと' do
-        
+        user = build(:user, beebits_name: '@bee!bits')
+        user.valid?
+        expect(user.errors.full_messages).to include(
+          'BeeBitsユーザー名 は英数字とアンダーバー(_)のみが使用できます'
+        )
       end
 
       it '@(アットマーク)が2つ以上含まれないこと' do
-        
+        user = build(:user, beebits_name: 'bee@@bits')
+        user.valid?
+        expect(user.errors.full_messages).to include('BeeBitsユーザー名 は英数字とアンダーバー(_)のみが使用できます')
       end
     end
 
@@ -118,22 +148,30 @@ RSpec.describe User, type: :model do
       end
 
       it '入力が8文字以上かつ英数字が含まれること' do
-        user = build(:user, password: 'bee83')
+        user = build(:user, password: 'BeeBitsPassword')
         user.valid?
         expect(user.errors.full_messages).to include('パスワード は英数字の組み合わせで8文字以上で入力してください')
       end
 
       it '入力が128文字以内であること' do
-        user = build(:user, password: 'bee83')
+        user = build(:user, password:
+          'beebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebitsbeebits
+          beebitsbeebitsbeebitsbeebitsbeebitsbeebits123'
+        )
         user.valid?
         expect(user.errors.full_messages).to include('パスワード は128文字以内で入力してください')
       end
 
       it '入力が確認用と一致すること' do
         user = build(:user, password: 'password123')
-        user = build(:user, password_confirmation: 'password123')
+        user = build(:user, password_confirmation: 'password1234')
         user.valid?
         expect(user.errors.full_messages).to include('パスワード(確認用) が一致しません')
+      end
+
+      it 'ユーザーのパスワードがハッシュ化されていること' do
+        user = create(:user, password: 'password123')
+        expect(user.encrypted_password).not_to eq('password123')
       end
     end
   end
