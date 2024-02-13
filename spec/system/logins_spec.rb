7d8504936f572863ac445cfe3a98b9d9c6a8da43
hiroaki_seto <hiroaki5141616@me.com>
2024-02-13 23:38:13 +0900

    ログイン挙動のsystemテストを追加

---
+++ spec/system/logins_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :system do
+
+  it '未ログインの場合ログインページへ遷移すること' do
+    visit root_path
+    expect(page).to have_text('ログイン')
+  end
+
+  context 'ログイン' do
+    let!(:user) do
+      User.create(
+        name: '蜜蜂太郎',
+        email: 'beebits@example.com',
+        phone_number: '08012345678',
+        birthdate: '1995-01-01',
+        beebits_name: '@Bee_Bits',
+        password: 'password123',
+        password_confirmation: 'password123'
+      )
+    end
+
+    it '正しい情報でログインができること' do
+      visit new_user_session_path
+      fill_in 'user_beebits_name', with: '@Bee_Bits'
+      fill_in 'user_password', with: 'password123'
+      click_button 'ログイン'
+      expect(page).to have_current_path(root_path)
+    end
+  end
+
+  describe 'ログアウト' do
+    let!(:user) do
+      User.create(
+        name: '蜜蜂太郎',
+        email: 'beebits@example.com',
+        phone_number: '08012345678',
+        birthdate: '1995-01-01',
+        beebits_name: '@Bee_Bits',
+        password: 'password123',
+        password_confirmation: 'password123'
+      )
+    end
+
+    it 'ログアウトができること' do
+      visit new_user_session_path
+      fill_in 'user_beebits_name', with: '@Bee_Bits'
+      fill_in 'user_password', with: 'password123'
+      click_button 'ログイン'
+      expect(page).to have_current_path(root_path)
+      click_button 'ログアウト'
+      expect(page).to have_current_path(new_user_session_path)
+    end
+  end
+end
