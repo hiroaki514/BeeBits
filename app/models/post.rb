035628d4ed54e1711cd97d4b38a4773241ada58a
hiroaki_seto <hiroaki5141616@me.com>
2024-02-21 15:20:22 +0900

    投稿機能のバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  belongs_to :user
+
+  validates :post_content,
+            presence: true,
+            length: { maximum: 140,
+            message: "は140文字以内で入力してください"
+            }
 end
