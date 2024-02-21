915d244ac2aef57c21e99f51f60857f13b4d5110
hiroaki_seto <hiroaki5141616@me.com>
2024-02-21 15:13:09 +0900

    記述の修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  belongs_to :user
-
-  validates :post_content,
-            presence: true,
-            length: { maximum: 140,
-            message: "は140文字以内で入力してください"
-            }
 end
