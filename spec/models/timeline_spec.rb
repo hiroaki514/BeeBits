60fe287462719c8a85c062510bdff80d70425106
hiroaki_seto <hiroaki5141616@me.com>
2024-02-22 01:18:21 +0900

    投稿機能のモデルテスト追加

--- spec/models/timeline_spec.rb
+++ spec/models/timeline_spec.rb
@@ -3,5 +3,72 @@
 require 'rails_helper'
 
 RSpec.describe Timeline, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validation' do
+    context 'テキストの場合' do
+      let(:user) { create(:user) }
+      let(:timeline) { create(:timeline, user: user) }
+
+      context '入力が0文字の場合' do
+        before do
+          timeline.update(content: '')
+        end
+
+        it '無効であること' do
+          expect(timeline).not_to be_valid
+        end
+      end
+
+      context '入力が1文字の場合' do
+        before do
+          timeline.update(content: 'あ')
+        end
+
+        it '有効であること' do
+          expect(timeline).to be_valid
+        end
+      end
+
+      context '入力が2文字の場合' do
+        before do
+          timeline.update(content: 'あい')
+        end
+
+        it '有効であること' do
+          expect(timeline).to be_valid
+        end
+      end
+
+      context '入力が139文字の場合' do
+        before do
+          timeline.update(content: 'あ' * 139)
+        end
+
+        it '有効であること' do
+          expect(timeline).to be_valid
+        end
+      end
+
+      context '入力が140文字の場合' do
+        before do
+          timeline.update(content: 'あ' * 140)
+        end
+
+        it '有効であること' do
+          expect(timeline).to be_valid
+        end
+      end
+
+      context '入力が141文字の場合' do
+        before do
+          timeline.update(content: 'あ' * 141)
+        end
+
+        it '無効であること' do
+          expect(timeline).not_to be_valid
+        end
+      end
+
+    end
+  end
+
 end
