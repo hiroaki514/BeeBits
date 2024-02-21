660bd69cf6c02869bce2bae50f8ba29f52488631
hiroaki_seto <hiroaki5141616@me.com>
2024-02-21 22:26:23 +0900

    投稿のバリデーションを追加

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -2,4 +2,12 @@
 
 class Timeline < ApplicationRecord
   belongs_to :user
+
+  validates :content,
+  presence: true,
+  length: {
+    minimum: 1,
+    maximum: 140,
+    message: "は140文字以内で入力してください"
+  }
 end
