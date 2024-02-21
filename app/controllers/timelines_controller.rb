95a61c1edb5f116b3998f77e89f1399fe769dcef
hiroaki_seto <hiroaki5141616@me.com>
2024-02-21 15:02:04 +0900

    Postモデルの作成

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
 class TimelinesController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to timelines_path, notice: '投稿が作成されました'
+    else
+      redirect_to timelines_path, alert: '投稿の作成に失敗しました'
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:body, :user_id, :post_content)
+  end
 end
