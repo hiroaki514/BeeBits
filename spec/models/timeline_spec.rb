75ec0023d42b73b9238419ffa96b371718b580e5
hiroaki_seto <hiroaki5141616@me.com>
2024-03-29 00:04:39 +0900

    削除機能のmodelsテストを追記

--- spec/models/timeline_spec.rb
+++ spec/models/timeline_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Timeline, type: :model do
   describe '#validation' do
-    context 'テキストの場合' do
+    context 'テキスト投稿の場合' do
       let(:user) { build(:user) }
       let(:timeline) { build(:timeline, user:, content:) }
 
@@ -55,6 +55,25 @@ RSpec.describe Timeline, type: :model do
           expect(timeline).not_to be_valid
         end
       end
+    end
+
+    context '投稿を削除する場合' do
+      let(:user) { build(:user) }
+      let(:timeline) { build(:timeline, user:, content:) }
+
+      context '投稿者本人の場合' do
+        it '削除できること' do
+          expect { timeline.destroy }.to change(Timeline, :count).by(-1)
+        end
+      end
+
+      context '他者の投稿の場合' do
+        it '削除できないこと' do
+          timeline.user = user
+          expect { timeline.destroy }.not_to change(Timeline, :count)
+        end
+      end
+
     end
   end
 end
