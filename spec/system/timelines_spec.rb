0fbb6ff8e308578da31066fb1441d9781bc52be8
hiroaki_seto <hiroaki5141616@me.com>
2024-03-29 00:17:17 +0900

    投稿削除機能のsystemテストを追記

--- spec/system/timelines_spec.rb
+++ spec/system/timelines_spec.rb
@@ -3,60 +3,77 @@
 require 'rails_helper'
 
 RSpec.describe 'Timelines', type: :system do
-  before do
-    create(:user)
-    visit new_user_session_path
-    fill_in 'BeeBitsユーザー名', with: '@bee_bits123'
-    fill_in 'パスワード', with: 'password123'
-    click_on 'ログイン'
-  end
+  describe '投稿' do
+    before do
+      create(:user)
+      visit new_user_session_path
+      fill_in 'BeeBitsユーザー名', with: '@bee_bits123'
+      fill_in 'パスワード', with: 'password123'
+      click_on 'ログイン'
+    end
 
-  context 'テキスト投稿の場合' do
-    context '0文字の場合' do
-      it '失敗すること' do
-        fill_in 'content', with: ''
-        click_on '投稿'
-        expect(page).to have_content '投稿に失敗しました'
+    context 'テキスト投稿の場合' do
+      context '0文字の場合' do
+        it '失敗すること' do
+          fill_in 'content', with: ''
+          click_on '投稿'
+          expect(page).to have_content '投稿に失敗しました'
+        end
+      end
+
+      context '1文字の場合' do
+        it '成功すること' do
+          fill_in 'content', with: '蜂'
+          click_on '投稿'
+          expect(page).to have_content '投稿が送信されました'
+        end
+      end
+
+      context '2文字の場合' do
+        it '成功すること' do
+          fill_in 'content', with: '蜜蜂'
+          click_on '投稿'
+          expect(page).to have_content '投稿が送信されました'
+        end
+      end
+
+      context '139文字の場合' do
+        it '成功すること' do
+          fill_in 'content', with: '蜂' * 139
+          click_on '投稿'
+          expect(page).to have_content '投稿が送信されました'
+        end
+      end
+
+      context '140文字の場合' do
+        it '成功すること' do
+          fill_in 'content', with: '蜂' * 140
+          click_on '投稿'
+          expect(page).to have_content '投稿が送信されました'
+        end
+      end
+
+      context '141文字の場合' do
+        it '失敗すること' do
+          fill_in 'content', with: '蜂' * 141
+          click_on '投稿'
+          expect(page).to have_content '投稿に失敗しました'
+        end
       end
     end
 
-    context '1文字の場合' do
-      it '成功すること' do
-        fill_in 'content', with: '蜂'
-        click_on '投稿'
-        expect(page).to have_content '投稿が送信されました'
-      end
-    end
+    context '投稿削除の場合' do
 
-    context '2文字の場合' do
-      it '成功すること' do
-        fill_in 'content', with: '蜜蜂'
-        click_on '投稿'
-        expect(page).to have_content '投稿が送信されました'
-      end
-    end
+      context '投稿者本人の場合' do
+        it '成功すること' do
 
-    context '139文字の場合' do
-      it '成功すること' do
-        fill_in 'content', with: '蜂' * 139
-        click_on '投稿'
-        expect(page).to have_content '投稿が送信されました'
+        end
       end
-    end
 
-    context '140文字の場合' do
-      it '成功すること' do
-        fill_in 'content', with: '蜂' * 140
-        click_on '投稿'
-        expect(page).to have_content '投稿が送信されました'
-      end
-    end
+      context '他者の投稿の場合' do
+        it '削除できないこと' do
 
-    context '141文字の場合' do
-      it '成功すること' do
-        fill_in 'content', with: '蜂' * 141
-        click_on '投稿'
-        expect(page).to have_content '投稿に失敗しました'
+        end
       end
     end
   end
