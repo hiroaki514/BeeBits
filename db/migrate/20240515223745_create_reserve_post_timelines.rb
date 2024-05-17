e79a38fb2eecb728792d4e45f2ea5026798493c4
hiroaki_seto <hiroaki5141616@me.com>
2024-05-17 11:51:53 +0900

    カラムのコメント記述箇所を修正

--- db/migrate/20240515223745_create_reserve_post_timelines.rb
+++ db/migrate/20240515223745_create_reserve_post_timelines.rb
@@ -1,7 +1,7 @@
 class CreateReservePostTimelines < ActiveRecord::Migration[7.1]
   def change
     create_table :reserve_post_timelines do |t|
-      t.text :content
+      t.text :content, null: false, comment: "予約投稿内容"
       t.timestamps
     end
   end
