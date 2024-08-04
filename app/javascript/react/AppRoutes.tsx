5f7ca4f920ff15fabb8e5420ef1d4c773a7c7bea
hiroaki_seto <hiroaki5141616@me.com>
2024-08-04 23:10:26 +0900

    404NotFoundページとルートの記述を追加

--- app/javascript/react/AppRoutes.tsx
+++ app/javascript/react/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Top } from './pages/Top';
+import { NotFound } from './pages/NotFound';
 
 export const AppRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Top />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   )
