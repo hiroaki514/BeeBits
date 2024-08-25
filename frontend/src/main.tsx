e69042c1a8a798f5dd3020db09a3b73a3f374469
hiroaki_seto <hiroaki5141616@me.com>
2024-08-26 01:13:34 +0900

    ProfileEdit.tsxの追加とプロフィール編集画面の移行

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import TimeLine from './TimeLine.tsx';
 import Profile from './Profile.tsx';
+import ProfileEdit from './ProfileEdit.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<Navigate to="/timelines" />} />
         <Route path="/timelines" element={<TimeLine />} />
         <Route path="/profiles/:userId" element={<Profile />} />
+        <Route path="/profiles/:userId/edit" element={<ProfileEdit />} />
       </Routes>
     </Router>
   </StrictMode>,
