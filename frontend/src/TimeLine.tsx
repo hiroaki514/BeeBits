0a1167478563cc2b31e218ad8367bfe44b03518f
hiroaki_seto <hiroaki5141616@me.com>
2024-09-02 22:28:35 +0900

    profiles関連ファイルを一度削除

--- frontend/src/TimeLine.tsx
+++ frontend/src/TimeLine.tsx
@@ -3,13 +3,24 @@ import styled from 'styled-components';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+const Container = styled.div`
+  padding: 20px;
+  background-color: #f5f5f5;
+  border-radius: 8px;
+`;
 
+const Header = styled.h1`
+  font-size: 24px;
+  color: #333;
+`;
 
 const TimeLine: React.FC = () => {
-
-  <>
-  </>
-
+  return (
+    <Container>
+      <Header>タイムライン</Header>
+      <p>ここにタイムラインのコンテンツが表示されます。</p>
+    </Container>
+  );
 };
 
 export default TimeLine;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,8 +8,6 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Navigate to="/timelines" />} />
         <Route path="/timelines" element={<TimeLine />} />
-        <Route path="/profiles/:userId" element={<Profile />} />
-        <Route path="/profiles/:userId/edit" element={<ProfileEdit />} />
       </Routes>
     </Router>
   </StrictMode>,
