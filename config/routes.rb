f036562d1d41406e17fd45f07475dbbd625bf4ac
hiroaki_seto <hiroaki5141616@me.com>
2024-09-13 09:26:10 +0900

    routes.rbをapi形式に記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,27 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Devise 認証用のルート
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
-  root 'timelines#index'
-  resources :timelines
-  resources :users, only: [:show] do
-    resource :profile, only: %i[show edit update], controller: 'profiles'
+  namespace :api do
+    namespace :v1 do
+      # Devise 認証用のルート
+      devise_for :users, controllers: {
+        registrations: 'users/registrations',
+        sessions: 'users/sessions'
+      }
+
+      resources :timelines
+      resources :users, only: [:show] do
+        resource :profile, only: %i[show edit update], controller: 'profiles'
+      end
+
+      post 'favorites/:id', to: 'favorites#create', as: 'add_to_favorites'
+      delete 'favorites/:id', to: 'favorites#destroy', as: 'destroy_favorite'
+
+      resources :reserve_post_timelines, only: [:create]
+    end
   end
-  post 'favorites/:id', to: 'favorites#create', as: 'add_to_favorites'
-  delete 'favorites/:id', to: 'favorites#destroy', as: 'destroy_favorite'
-  resources :reserve_post_timelines, only: [:create]
+
+  # フロントエンドのルートをすべてReactに任せる
+  root 'timelines#index'
+  get '*path', to: 'timelines#index', constraints: ->(request) { !request.xhr? && request.format.html? }
 end
